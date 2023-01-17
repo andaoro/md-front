c3a816cae200385115aa75cc98356f5e254b1427
andres <andaoro1@gmail.com>
2023-01-17 18:00:49 -0500

    profile

---
+++ src/ProfileOptions/BannerImages.js
@@ -0,0 +1,35 @@
+export const arrayBanners =[
+    'https://i.pinimg.com/564x/f0/5a/cf/f05acf924d6fb8c6414ee51b99a02ee7.jpg',
+    'https://i.pinimg.com/564x/f5/b9/bb/f5b9bb9099f51fb1d218cbfc7046a35d.jpg',
+    'https://i.pinimg.com/564x/5f/df/62/5fdf625a2fd156517b84e9fa51246325.jpg',
+    'https://i.pinimg.com/564x/45/7b/2a/457b2ad7b92de3ec17165573de041915.jpg',
+    'https://i.pinimg.com/564x/dc/ca/3d/dcca3d7b6cf56c45ede55fd341334c5a.jpg',
+    'https://i.pinimg.com/564x/7e/54/48/7e544881666d8bc94423a135f748f9ae.jpg',
+    'https://i.pinimg.com/564x/5b/bc/dd/5bbcdd4d749f900c11a2ac5e47cc505e.jpg',
+    'https://i.pinimg.com/564x/c8/7f/6d/c87f6dca39361f306709d8dcd463c855.jpg',
+    'https://i.pinimg.com/originals/8c/c2/93/8cc293997d61acb13f68f53c62f786dd.gif',
+    'https://i.pinimg.com/originals/1f/73/60/1f736040a3868b98c8c4fb9146a7b955.gif',
+    'https://i.pinimg.com/originals/2c/54/88/2c5488fd350f9317df2a50afb6ae5d54.gif',
+    'https://i.pinimg.com/originals/c2/39/78/c239781a884d363b6ee17280606b671f.gif',
+    'https://i.pinimg.com/564x/0d/92/2a/0d922a8b9e787808ee901b8078b6265e.jpg',
+    'https://i.pinimg.com/564x/a8/79/8d/a8798da1f8c9334d7bce5658cb6b9477.jpg',
+    'https://i.pinimg.com/564x/24/e1/9f/24e19fc23f7dce27c9b83a6e06911500.jpg'
+]
+
+export const arrayIcons = [
+    'https://i.pinimg.com/564x/ef/d0/3c/efd03cacfff7b6034426d511b2b37921.jpg',
+    'https://i.pinimg.com/564x/7e/4d/87/7e4d87a34a2e488d87c719c9161cf262.jpg',
+    'https://i.pinimg.com/564x/19/0d/80/190d807fccf916bedd598d9374cef76c.jpg',
+    'https://i.pinimg.com/564x/71/02/d7/7102d7c13f58776baa792f2ec0934093.jpg',
+    'https://i.pinimg.com/564x/e0/9f/e4/e09fe4a2c06b441c683f7dc58f176c4b.jpg',
+    'https://i.pinimg.com/564x/c6/a6/24/c6a624424db6cb2900bad759026ab346.jpg',
+    'https://i.pinimg.com/564x/6b/7f/75/6b7f75a7b2e997cf4ae384ae10c6d148.jpg',
+    'https://i.pinimg.com/564x/75/e7/9b/75e79b046005e225cbd63849ef402bf4.jpg',
+    'https://i.pinimg.com/564x/15/49/0f/15490f029987fca190401c457e8bc2da.jpg',
+    'https://i.pinimg.com/564x/f3/d4/3e/f3d43ec3efe487a09079a733f6f50464.jpg',
+    'https://i.pinimg.com/564x/01/2a/a1/012aa1146a6b79250f3a03e09bed00db.jpg',
+    'https://i.pinimg.com/564x/30/3a/2b/303a2bdb30e8eb8774e93f5049f257a3.jpg',
+    'https://i.pinimg.com/564x/b1/b5/c8/b1b5c8b96be5bce581640bc70622fb7d.jpg',
+    'https://i.pinimg.com/564x/fe/32/a1/fe32a1a0422f0a1396f45b2f967dd794.jpg',
+    'https://i.pinimg.com/736x/6d/96/7b/6d967b7a967812eca7323ecbc50994ba.jpg'
+]
--- src/ProfileOptions/Profile.js
+++ src/ProfileOptions/Profile.js
@@ -1,64 +1,111 @@
 import React, { useState } from 'react'
 import { Navbar } from '../components/Navbar/Navbar'
 import './Profile.css'
-import { ChromePicker } from 'react-color';
+import { SketchPicker } from 'react-color';
+import { arrayBanners, arrayIcons } from './BannerImages'
 
 export const Profile = () => {
     const [color, setcolor] = useState('#FFFFFF')
+    const [bannerImg, setbannerImg] = useState('')
+    const [iconImg, seticonImg] = useState('')
+    const [colorFontCard, setcolorFontCard] = useState('#000000')
 
-    const arrprueba =[]
-
-    for (let index = 0; index < 150; index++) {
-        arrprueba.push(1) 
+    const saveProfile = () =>{
+        
     }
 
-    console.log(arrprueba)
 
+    return (
+        <div>
+            <Navbar />
+            <div className='container-Profile'>
+                <div className='profile-personalization'>
+                    <div className='Profile-BannerSelected banners'>
+                        <span className='profile-title'>Seleccione Banner de usuario</span>
+                        <div className='Profile-BannerGrid'>
+                            {
+                                arrayBanners.map((banner, index) => (
+                                    <div className='bannerImages' key={index} onClick={() => setbannerImg(banner)}>
+                                        <img src={banner} />
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <hr className='salto-linea' />
+                    <div className='Profile-BannerSelected icons'>
+                        <span className='profile-title'>Seleccione icono de usuario</span>
+                        <div className='Profile-BannerGrid'>
+                            {
+                                arrayIcons.map((banner, index) => (
+                                    <div className='bannerImages' key={index} onClick={() => seticonImg(banner)}>
+                                        <img src={banner} />
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <hr className='salto-linea' />
+                    <div className='profile-ColorCard'>
+                        <div>
+                            <span>Color opciones de perfil</span>
+                            <SketchPicker
+                                color={color}
+                                onChange={updateColor => setcolor(updateColor.hex)}
+                                width='30%'
+                                height='50%'
+                            />
+                        </div>
+                        <div>
+                            <span>Color opciones de perfil</span>
+                            <SketchPicker
+                                color={colorFontCard}
+                                onChange={updateColor => setcolorFontCard(updateColor.hex)}
+                                width='30%'
+                                height='50%'
+                            />
+                        </div>
 
-  return (
-    <div>
-        <Navbar/>
-        <div className='container-Profile'>
-            <div className='profile-personalization'>
-                <div className='Profile-BannerSelected'>
-                    <span className='profile-title'>Seleccione Banner de usuario</span>
-                    <div className='Profile-BannerGrid'>
-                        {
-                            arrprueba.map((banner)=>(
-                                <div className='bannerImages'>
-
-                                </div>
-                            ))
-                        }
+                    </div>
+                    <hr className='salto-linea' />
+                    <div className='Actions-UserProfile'>
+                        <div className='buttons-Actions'>
+                            <button className={`btnSave`}>
+                                <span>
+                                    Guardar
+                                </span>
+                            </button>
+                            <button className={`btncancel`}>
+                                <span>
+                                    Cancelar
+                                </span>
+                            </button>
+                        </div>
                     </div>
                 </div>
-                <hr className='salto-linea'/>
-                <div className='Profile-BannerSelected'>
-                    <span className='profile-title'>Seleccione icono de usuario</span>
-                    <div className='Profile-BannerGrid'>
-                        {
-                            arrprueba.map((banner)=>(
-                                <div className='bannerImages'>
-
-                                </div>
-                            ))
-                        }
+                <div className='Profile-card-user'>
+                    <div className='card-User' style={{ backgroundColor: color }}>
+                        <div className='card-User-Images'>
+                            <div className='bannerImage-cardUser'>
+                                <img src={bannerImg || arrayBanners[0]} />
+                            </div>
+                            <div className='iconImage-cardUser'>
+                                <img src={iconImg || arrayIcons[0]} />
+                            </div>
+                        </div>
+                        <div className='card-User-Body' >
+                            <div className='inputRowCard'>
+                                <input type={'text'} placeholder='Usuario' className='inputCardUser' style={{color:colorFontCard}}/>
+                                <input type={'text'} placeholder='Nro.Contacto' className='inputCardUser' style={{color:colorFontCard}}/>
+                            </div>
+                            <div className='inputRowCard'>
+                                <input type={'number'} placeholder='Edad' className='inputCardUser' style={{color:colorFontCard}}/>
+                                <input type={'email'} placeholder='Correo' className='inputCardUser' style={{color:colorFontCard}}/>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <hr className='salto-linea'/>
-                <div className='profile-ColorCard'>
-                    <span>Color opciones de perfil</span>
-                    <ChromePicker
-                    color={color}
-                    onChange={updateColor => setcolor(updateColor.hex)}
-                    />
-                </div>
-
-            </div>
-            <div className='Profile-card-user'>
-
             </div>
         </div>
-    </div>
-  )
+    )
 }
